83ab83290d91dd3dfa0f1d8883c5920904c64cce
xYamii <32822766+xYamii@users.noreply.github.com>
2021-02-17 21:05:55 +0100

    temp

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-$(window).on("load", () => {
-  const url = "https://api.jsonbin.io/b/602ad51499ac3873a34a098a";
-  let htmlbody = "";
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((element) => {
-        htmlbody += `<tr scope="row"><th>${element.id}</th><td>${element.sound}</td></tr>`;
-      });
-      $("#soundTable").html(htmlbody);
-    });
-});
+//$(window).on("load", () => {
+//   const url = "https://api.jsonbin.io/b/602ad51499ac3873a34a098a";
+//   let htmlbody = "";
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       data.forEach((element) => {
+//         htmlbody += `<tr scope="row"><th>${element.id}</th><td>${element.sound}</td></tr>`;
+//       });
+//       $("#soundTable").html(htmlbody);
+//     });
+// });
